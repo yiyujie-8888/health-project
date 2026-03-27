ae7ddb164cfb59590ddf7a0a69196f1f82b4813f
Yi yujie <3241806077@qq.com>
2026-03-27 15:08:20 +0800

    提交con

--- health-project/health-backend/src/main/java/com/health/common/filter/JwtAuthenticationFilter.java
+++ health-project/health-backend/src/main/java/com/health/common/filter/JwtAuthenticationFilter.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.util.Collections;
 
 /**
  * JWT 认证过滤器
