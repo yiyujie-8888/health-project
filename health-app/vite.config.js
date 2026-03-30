d859800f746986256c3061963e13becc5c6a881a
lllllylllllc <1187151507@qq.com>
2026-03-31 01:17:01 +0800

    Add files via upload

--- health-app/vite.config.js
+++ health-app/vite.config.js
@@ -1,14 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteMockServe } from 'vite-plugin-mock'
 import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
-  plugins: [
-    vue()
-  ],
+  plugins: [vue(), viteMockServe({
+    mockPath: path.resolve(__dirname, './mock'),
+    localEnabled: true,
+    supportTs: false,
+    logger: false
+  })],
+  // 路径别名配置（关键：让 @ 指向 src）
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      '@': path.resolve(__dirname, './src')
     }
   }
 })
